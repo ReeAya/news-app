4698733a464f1ddfd40ef2788a15ad77a602a417
reezan_s <reezansmith@gmail.com>
2024-07-16 14:51:48 +0900

    Added some styling

--- scripts.js
+++ scripts.js
@@ -16,11 +16,26 @@ async function fetchNews() {
 function displayNews(articles) {
   const newsDiv = document.querySelector("#news");
   for (const article of articles) {
-    const articleDiv = document.createElement("div");
-
+    console.log(article);
+    const articleDiv = document.createElement("a");
+    articleDiv.style.display = "block";
+    articleDiv.href = article.url;
+    articleDiv.classList.add("card");
+    articleDiv.style.width = "18rem";
+    articleDiv.style.padding = "10px";
+    articleDiv.style.textDecoration = "none";
+    const articleBody = document.createElement("div");
+    const articleImg = document.createElement("img");
+    articleImg.classList.add("card-img-top");
+    articleImg.src = article.urlToImage;
+    articleBody.classList.add("card-body");
+    articleDiv.appendChild(articleBody);
+    articleBody.appendChild(articleImg);
     //create and append a headline to the articleDiv
-    const title = document.createElement("h4");
+
+    const title = document.createElement("h5");
     title.textContent = article.title;
+    title.classList.add("card-title");
     articleDiv.appendChild(title);
 
     // TODO: Use document.createElement and appendChild to create and append more elements
